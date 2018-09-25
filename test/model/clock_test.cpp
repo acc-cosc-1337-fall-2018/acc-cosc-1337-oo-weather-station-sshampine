da066960e29b913ad1120dcbf7c1d1ea7bcb96da
Scott Shampine <sshampine@gmail.com>
2018-09-24 19:48:56 -0500

    updated unit tests

--- test/model/clock_test.cpp
+++ test/model/clock_test.cpp
@@ -6,7 +6,7 @@ TEST_CASE("Test get hours from seconds")
 {
 	Clock clock;
 	
-	REQUIRE(clock.get_hours(3600 == 1));
+	REQUIRE(clock.get_hours(3600) == 1);
 	REQUIRE(clock.get_hours(34950) == 9);
 }
 
@@ -14,7 +14,7 @@ TEST_CASE("Test get minutes from seconds")
 {
 	Clock clock;
 
-	REQUIRE(clock.get_minutes(3600 == 0));
+	REQUIRE(clock.get_minutes(3600) == 0);
 	REQUIRE(clock.get_minutes(3800) == 3);
 	REQUIRE(clock.get_minutes(34950) == 42);
 }
@@ -23,7 +23,7 @@ TEST_CASE("Test get seconds from seconds since 1970")
 {
 	Clock clock;
 
-	REQUIRE(clock.get_seconds(3600 == 0));
+	REQUIRE(clock.get_seconds(3600) == 0);
 	REQUIRE(clock.get_seconds(3800) == 20);
 	REQUIRE(clock.get_seconds(34950) == 30);
 }
