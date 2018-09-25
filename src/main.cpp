a3fbf135bd05a97e8cb36766675f789f75f2a24e
scott shampine <sshampine@gmail.com>
2018-09-25 18:54:55 -0500

    updated main

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,22 @@
 #include "clock.h"
+#include<iostream>
+#include<thread>
 
 int main() 
 {
 	Clock clock;
+	
 
-	clock.display_time();
+	int i{0};
+
+	while (i != 100)
+	{
+		std::this_thread::sleep_for(std::chrono::seconds(1));
+		clock.display_time();
+		std::cout << std::endl;
+		i++;
+	}
+	
 
 	return 0;
 }
